b8083f256ce1b0c99c673763b88c9917b91b451b
Aurelien <masiero.aurelien@outlook.fr>
2023-07-02 01:57:09 +0200

    Terre15

---
+++ terre15.rb
@@ -0,0 +1,6 @@
+#Well done !
+
+positive = ["génial", "super", "amusant", "parfois facile, parfois pas du tout"]
+quote = positive.sample
+
+puts "J'ai terminé l'épreuve de la Terre et c'était #{quote}!"
