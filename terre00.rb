3659330d1482a1dbf1f78afd316bf88dad64eb23
Aurelien <masiero.aurelien@outlook.fr>
2023-07-01 15:09:49 +0200

    terre00.rb validé

---
+++ terre00.rb
@@ -0,0 +1,12 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+count = 0
+
+=begin
+  puts alphabet[0..25]
+=end
+
+while count < 26
+  puts alphabet[count]
+  count = count + 1
+end
