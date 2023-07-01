9de39493f808e19257192b333c3e75889d7e6037
Aurelien <masiero.aurelien@outlook.fr>
2023-07-01 23:09:07 +0200

    MAJ Terre08

--- terre08.rb
+++ terre08.rb
@@ -1,15 +1,20 @@
-#Créez un programme qui affiche le résultat et le reste d’une division entre deux nombres.
 num1 = ARGV[0].to_i.to_s
 num2 = ARGV[1].to_i.to_s
 
 if num1 != nil && num2 != nil
 
-  num1 = num1.to_i
-  num2 = num2.to_i
+num2 = num2.to_i
 
-  power = num1 ** num2
-  puts "Result:", power
+  if num2 > 0
 
+    num1 = num1.to_i
+    num2 = num2.to_i
+
+    power = num1 ** num2
+    puts "Result:", power
+  else
+    puts "Erreur"
+  end
 else
   puts "Erreur"
 end
